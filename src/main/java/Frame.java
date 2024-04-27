32caf4d6cf4ebc713b601fa073cec290c2aa105a
SimonHahm <simon.hahm@t-online.de>
2024-04-27 10:55:42 +0200

    Finished tests for single Frames. In current state tests are still failing, because Frame.java is not implemented yet.
    Added javadoc comments for functions in Frame.java.

--- src/main/java/Frame.java
+++ src/main/java/Frame.java
@@ -1,15 +1,46 @@
 public class Frame {
     private int score;
+    private boolean strike;
+    private boolean spare;
 
+    /**
+     * Creates a new Frame, and needs the number of pins knocked over.
+     * @param throw1 number of pins knocked over in the first throw
+     * @param throw2 number of pins knocked over in the second throw
+     */
     Frame(int throw1, int throw2) {
 
     }
 
+    /**
+     * Returns the current score of the frame.
+     * @return the current score of the frame
+     */
     public int getScore() {
         return score;
     }
 
-    public void setScore(int score) {
-        this.score = score;
+    /**
+     * Adds score to a frame i.e. if Frame was a strike or a spare.
+     * @param addedScore The score that is added to this frame due to later throws
+     */
+    public void addScore(int addedScore) {
+        this.score += addedScore;
+    }
+
+    /**
+     * Shows, if current frame is a spare.
+     * @return boolean, that shows if the frame is a spare
+     */
+    public boolean isSpare() {
+        return spare;
+    }
+
+    /**
+     * Shows, if current frame is a strike.
+     * @return boolean, that shows if the frame is a strike
+     */
+    public boolean isStrike() {
+        return strike;
     }
 }
--- src/test/java/FrameTest.java
+++ src/test/java/FrameTest.java
@@ -1,12 +1,39 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class FrameTest {
     @Test
-    public void testFrameConstructor() {
+    public void testConstructor() {
+        Frame testFrame = new Frame(4,2);
+        assertEquals(6,testFrame.getScore());
+    }
+
+    @Test
+    public void testStrike() {
+        Frame testFrame = new Frame(10,0);
+        assertEquals(10,testFrame.getScore());
+        assertTrue(testFrame.isStrike());
+        assertFalse(testFrame.isSpare());
+    }
+
+    @Test
+    public void testSpare() {
         Frame testFrame = new Frame(4,6);
         assertEquals(10,testFrame.getScore());
+        assertTrue(testFrame.isSpare());
+        assertFalse(testFrame.isStrike());
+    }
+
+    /*
+    Might be overdone, but tests, if scores are added correctly. (In Fact this is not even
+    part of TDD, because i programmed the "addScore" function before i created this test.)
+     */
+    @Test
+    public void testScoreAddition(){
+        Frame testFrame = new Frame(4,6);
+        testFrame.addScore(4);
+        assertEquals(14,testFrame.getScore());
 
     }
 }
