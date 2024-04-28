165d29dea4ece4efe621affd67f685a90121c516
SimonHahm <simon.hahm@t-online.de>
2024-04-28 19:13:18 +0200

    added testfunctions for calculating the score of one frame taking account of frame dependencies.

--- src/test/java/LineTest.java
+++ src/test/java/LineTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LineTest {
@@ -22,5 +25,47 @@ class LineTest {
 
         int[] rolls6 = {8,1};
         assertArrayEquals(rolls6, Line.parseInput("81"));
+    }
+
+    @Test
+    public void scoreCalcTestOnlyStrikes() {
+        Line line = new Line();
+        line.setFrames(new ArrayList<>(List.of()));
+
+        line.frameScoreCalc(new int[]{10,0});
+        assertEquals(10, line.getFrames().get(0).getScore());
+
+        line.frameScoreCalc(new int[]{10,0});
+        assertEquals(10, line.getFrames().get(1).getScore());
+        assertEquals(20, line.getFrames().get(0).getScore());
+
+        line.frameScoreCalc(new int[]{10,0});
+        assertEquals(10, line.getFrames().get(2).getScore());
+        assertEquals(20, line.getFrames().get(1).getScore());
+        assertEquals(30, line.getFrames().get(0).getScore());
+
+        line.frameScoreCalc(new int[]{10,0});
+        assertEquals(10, line.getFrames().get(3).getScore());
+        assertEquals(20, line.getFrames().get(2).getScore());
+        assertEquals(30, line.getFrames().get(1).getScore());
+        assertEquals(30, line.getFrames().get(0).getScore());
+    }
+
+    @Test
+    public void scoreCalcTest() {
+        Line line = new Line();
+        line.setFrames(new ArrayList<>(List.of()));
+
+        line.frameScoreCalc(new int[]{6,2});
+        assertEquals(8, line.getFrames().get(0).getScore());
+
+        line.frameScoreCalc(new int[]{5,5});
+        assertEquals(10, line.getFrames().get(1).getScore());
+        assertEquals(8, line.getFrames().get(0).getScore());
+
+        line.frameScoreCalc(new int[]{7,0});
+        assertEquals(7, line.getFrames().get(2).getScore());
+        assertEquals(17, line.getFrames().get(1).getScore());
+        assertEquals(8, line.getFrames().get(0).getScore());
     }
 }
