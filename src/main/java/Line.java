4f768a6ead684ef9cfcfe5309e70f1a24f0d3aca
SimonHahm <simon.hahm@t-online.de>
2024-04-28 19:14:54 +0200

    implemented score calculateScore() to calculate score of one frame.

--- src/main/java/Line.java
+++ src/main/java/Line.java
@@ -9,6 +9,34 @@ public class Line {
         }
     }
 
+    /**
+     * Calculates score of frames when new frame has been entered. Also takes in account frame interdependencies.
+     * @param scoreCurrentFrame the score of the two rolls of the entered frame
+     */
+    public void frameScoreCalc(int[] scoreCurrentFrame) {
+        Frame currentFrame = new Frame(scoreCurrentFrame[0], scoreCurrentFrame[1]);
+        frames.add(currentFrame);
+        int frameNumber = frames.size() - 1;  //
+        if (frameNumber >= 1 && frameNumber<=9) { //no further score calculation for first frame special case for 11th
+                                                   //and 12th frame
+            if (frames.get(frameNumber - 1).isStrike()) {
+                //previous frame was strike
+                frames.get(frameNumber - 1).addScore(currentFrame.getScore());
+                if (frameNumber >= 2 && frames.get(frameNumber - 2).isStrike()) {
+                    //last two frames were strikes.
+                    frames.get(frameNumber - 2).addScore(currentFrame.getScore());
+                }
+            } else if (frames.get(frameNumber - 1).isSpare()) {
+                //last frame was spare, hence add score of first roll to last frame
+                frames.get(frameNumber - 1).addScore(scoreCurrentFrame[0]);
+            }
+        } else if (frameNumber == 10) { //if the ninth frame is a strike, the score of the 11th frame is added.
+            if (frames.get(frameNumber-2).isStrike()) {
+                frames.get(frameNumber - 2).addScore(currentFrame.getScore());
+            }
+        }
+    }
+
     static int[] parseInput(String input) {
         if (input.equalsIgnoreCase("X")) {
             //strike: that means the first roll knocked all pins over.
@@ -41,5 +69,20 @@ public class Line {
         }
         System.out.println("Invalid input. Is counted as no score"); //may be changed to throw an exception
         return new int[]{0, 0}; //This statement is only reached, if invalid input is given.
+    }
+
+    private void calculateScore() {
+        for (Frame frame : frames) { //should I create a testcase for this function?
+            lineScore += frame.getScore();
+        }
+    }
+
+
+    public ArrayList<Frame> getFrames() {
+        return frames;
+    }
+
+    public void setFrames(ArrayList<Frame> frames) {
+        this.frames = frames;
     }
 }
